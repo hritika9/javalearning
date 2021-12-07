12b88b9db5b5fc7b59d44239934ba21b84d19c89
hritika9 <94508484+hritika9@users.noreply.github.com>
2021-12-07 12:28:59 +0530

    Add files via upload

---
+++ contructors.java
@@ -0,0 +1,36 @@
+package LEARNING;
+
+import learningjava.com.Machine;
+
+class Machine{
+	private String name;
+	private int age;
+	public Machine(){
+		this("hritika");
+	System.out.println("contructor running 1st");
+	}
+	public Machine(String name) {
+		this.name= name;
+		System.out.println("2nd running");
+	}
+	public Machine(String name , int age) {
+		this("hritika");
+		this.name=name;
+		this.age=age;
+		System.out.println("3rd constructor running");
+	}
+}
+
+
+public class contructors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Machine machine1 = new Machine();
+		Machine machine2 = new Machine("rohit");
+		Machine machine3 = new Machine("rohit",12);
+		
+
+	}
+
+}
